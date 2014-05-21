cd6ed891eed6b3c3d60dc37684e4ec053cfa84df
Matt Guindin <matt@guindin.com>
2014-05-21 15:19:57 -0400

    fixes

--- lunch.go
+++ lunch.go
@@ -1,22 +1,42 @@
-package lunch
+package main
 
 import (
     "io/ioutil"
 	"strings"
+	"fmt"
 )
 
+type Lunch struct {
+	yelp_url, radius, location, cuisine string
+	debug bool
+	rating, rev, choice int8
+}
+
 func main() {
-    radius := "0.5"
-    location := "10021"
-    debug := false
-    cuisine := "random"
-    yelp_url := "http://api.yelp.com/business_review_search?"
-    rating, rev := 0, 0
+	lunch := Lunch{
+		radius : "0.5",
+		location : "10021",
+		debug : false,
+		cuisine : "random",
+		yelp_url : "http://api.yelp.com/business_review_search?",
+		rating : 0,
+		rev : 0,
+		choice : 1}
+	fmt.Print(BuildYelpURL(lunch))
 }
 
 func GetYelpKey() string {
-    yelp_key := ""
     b, err := ioutil.ReadFile("yelp_key")
     if err != nil { panic(err) }
     return strings.Replace(string(b), "\n", "", 1)
+}
+
+func BuildYelpURL(lunch Lunch) string {
+	return lunch.yelp_url +
+		"term=" + lunch.cuisine +
+		"&location=" + lunch.location +
+		"&radius=" + lunch.radius +
+		"&limit=20" +
+		"&ywsid=" + GetYelpKey() +
+		"&category=restaurants"
 }
