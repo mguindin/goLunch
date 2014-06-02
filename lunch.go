62f7ff219009438c7fcde15cb8a11a94ac788407
Matt Guindin <matt@guindin.com>
2014-06-02 15:02:58 -0400

    changing certain functions to be methods to cut down on code complexity

--- lunch.go
+++ lunch.go
@@ -3,21 +3,70 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/mguindin/lunch/third_party/github.com/codegangsta/cli"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
-	"strings"
 	"strconv"
-	"math"
-	"github.com/mguindin/lunch/third_party/github.com/codegangsta/cli"
+	"strings"
 )
 
 type Lunch struct {
 	yelp_url, radius, location, cuisine string
 	debug                               bool
 	rating                              int8
-	rev									map[string] interface{}
+	rev                                 map[string]interface{}
 	choice                              int
+}
+
+func (lunch *Lunch) buildYelpUrl() string {
+	return lunch.yelp_url +
+		"term=" + lunch.cuisine +
+		"&location=" + lunch.location +
+		"&radius=" + lunch.radius +
+		"&limit=20" +
+		"&ywsid=" + GetYelpKey() +
+		"&category=restaurants"
+}
+
+func (lunch *Lunch) makeRequest() []byte {
+	resp, err := http.Get(lunch.buildYelpUrl())
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	//fmt.Print(string(body))
+	return body
+}
+
+func (lunch *Lunch) processYelpReturn(ret []byte) {
+	var dat map[string]interface{}
+	err := json.Unmarshal(ret, &dat)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	if lunch.debug {
+		fmt.Printf("businesses %+v\n", dat["businesses"])
+		fmt.Printf("message %+v\n", dat["message"])
+	}
+	businesses := dat["businesses"].([]interface{})
+	restaurant := businesses[0].(map[string]interface{})
+	fmt.Println("You will be having " + restaurant["name"].(string) +
+		", which is located at " + restaurant["address1"].(string) + ".")
+	rating := restaurant["avg_rating"].(float64)
+	fmt.Println(restaurant["name"].(string) + " has a rating of " + strconv.FormatFloat(rating, 'f', -1, 64))
+	reviews := restaurant["reviews"].([]interface{})
+	for _, val := range reviews {
+		m := val.(map[string]interface{})
+		if math.Abs(rating-m["rating"].(float64)) < 1.0 {
+			lunch.rev = m
+		}
+	}
+	review_text := strings.Replace(lunch.rev["text_excerpt"].(string), "\n", " ", -1)
+	fmt.Println("People are saying: " + review_text)
+
 }
 
 func main() {
@@ -31,7 +80,7 @@ func main() {
 			Name:      "search",
 			ShortName: "s",
 			Usage:     "Search for lunch",
-			Flags : []cli.Flag{
+			Flags: []cli.Flag{
 				cli.BoolFlag{"debug", "Output request URL"},
 				cli.StringFlag{"cuisine", "random", "cuisine to search for"},
 				cli.StringFlag{"radius", "0.5", "Radius for search"},
@@ -57,38 +106,10 @@ func run(c *cli.Context) {
 		rev:      make(map[string]interface{}),
 		choice:   c.Int("choice")}
 	if lunch.debug {
-		fmt.Println(BuildYelpURL(lunch))
+		fmt.Println(lunch.buildYelpUrl())
 		fmt.Printf("%+v\n", lunch)
 	}
-	ProcessYelpReturn(MakeRequest(BuildYelpURL(lunch)), lunch)
-}
-
-func ProcessYelpReturn(ret []byte, lunch Lunch) {
-	var dat map[string]interface{}
-	err := json.Unmarshal(ret, &dat)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	if (lunch.debug) {
-		fmt.Printf("businesses %+v\n", dat["businesses"])
-		fmt.Printf("message %+v\n", dat["message"])
-	}
-	businesses := dat["businesses"].([]interface{})
-	restaurant := businesses[0].(map[string]interface{})
-	fmt.Println("You will be having " + restaurant["name"].(string) +
-			", which is located at " + restaurant["address1"].(string) + ".")
-	rating := restaurant["avg_rating"].(float64)
-	fmt.Println(restaurant["name"].(string) + " has a rating of " + strconv.FormatFloat(rating, 'f', -1, 64))
-	reviews := restaurant["reviews"].([]interface{})
-	for _, val := range reviews {
-		m := val.(map[string] interface{})
-		if (math.Abs(rating - m["rating"].(float64)) < 1.0) {
-			lunch.rev = m
-		}
-	}
-	review_text := strings.Replace(lunch.rev["text_excerpt"].(string), "\n", " ", -1)
-	fmt.Println("People are saying: " + review_text)
-
+	lunch.processYelpReturn(lunch.makeRequest())
 }
 
 func GetYelpKey() string {
@@ -97,25 +118,4 @@ func GetYelpKey() string {
 		panic(err)
 	}
 	return strings.Replace(string(b), "\n", "", 1)
-}
-
-func BuildYelpURL(lunch Lunch) string {
-	return lunch.yelp_url +
-		"term=" + lunch.cuisine +
-		"&location=" + lunch.location +
-		"&radius=" + lunch.radius +
-		"&limit=20" +
-		"&ywsid=" + GetYelpKey() +
-		"&category=restaurants"
-}
-
-func MakeRequest(url string) []byte {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	//fmt.Print(string(body))
-	return body
 }
